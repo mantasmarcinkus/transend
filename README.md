f90c2518661f8d4eca79ddeac4e3a6fa727d0dfa
mantasmarcinkus <mantas.marcinkus.bio@gmail.com>
2017-04-05 21:50:44 +0100

    Add registry key registration to persist Toasts in Action center. Update Readme.

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
+## tran/send
+The project idea I got was when I needed to get a link from my phone to my computer and I was already looking for a side project to develop (I usually did using this kind of route:  
+Phone -> Share to Google Keep -> Open Browser -> Google Keep web page -> Click on the link.  
+
+Note: I haven't looked for application that can do it, because I wanted to do it :)  
+## How to run
 To jump start this project:  
 [https://developers.google.com/drive/v3/web/quickstart/nodejs](https://developers.google.com/drive/v3/web/quickstart/nodejs)  
 - **client_secret.json** should be obtained  
+Run these commands: 
 - `npm run register` in the console, copy the url to browser and then paste authentication token to console
-- `npm start`
 - `npm run reset` will reset the database file with a fake test notification
+- `npm run install-service` installs window service and registers registry that toast in Action center persist after retrieved from data source
 
-### Notifications in Action Center
-If you want for notifications to persist in Action Center (windows 10) you have to allow SnoarToast.exe notificatios to stay there  
-![SnoarToast](./ActionCenter.png)
-[Notifications in Action Center](http://stackoverflow.com/questions/31772192/toastnotifications-sent-from-powershell-disappear-from-action-center/39564998#39564998)
-### API
-TO DO: fill in data
+## Architecture
+![Architecture](./transend.png)
+
+## Stack
+Service:
+- Node.js
+
+Data storage:
+- Google Drive
+
+(TODO) Mobile app:
+- React native
+
+(TODO) Web app
+- Client side:
+  + React, Vuejs
+- Server side:
+  + Node.js, .NET Core
+
--- install.js
+++ install.js
@@ -1,19 +1,51 @@
 const Service = require('node-windows').Service;
+const regedit = require('regedit');
 
-// Create a new service object
-const svc = new Service({
-  name: 'transend',
-  description: 'Checks google drive app data and finds if there are new links and notifies windows desktop about them',
-  script: './index.js',
-  env: {
-    name: 'HOME',
-    value: process.env.USERPROFILE,
-  },
-});
-// Listen for the "install" event, which indicates the
-// process is available as a service.
-svc.on('install', () => {
-  svc.start();
-});
+const registryKey = 'HKCU\\SOFTWARE\\MICROSOFT\\WINDOWS\\CurrentVersion\\Notifications\\Settings\\transend';
+(() => {
+  new Promise((resolve) => {
+    regedit.createKey(registryKey, () => {
+      resolve();
+    });
+  })
+    .then(() => new Promise((resolve) => {
+      const registryValue = { [registryKey]: {
+        ShowInActionCenter: {
+          value: 1,
+          type: 'REG_DWORD',
+        },
+      } };
 
-svc.install();
+      regedit.putValue(registryValue, () => {
+        console.log('Added registry configuration for toaster to persist in Action center');
+        resolve();
+      });
+    }))
+    .then(() => new Promise((resolve) => {
+      regedit.list(registryKey,
+        (err = '') => {
+          console.log('Registry in this key: ', registryKey, err);
+          resolve();
+        });
+    }))
+    .then(() => {
+      // Create a new service object
+      const svc = new Service({
+        name: 'transend',
+        description: 'Checks google drive app data and finds if there are new links and notifies windows desktop about them',
+        script: './index.js',
+        env: {
+          name: 'HOME',
+          value: process.env.USERPROFILE,
+        },
+      });
+      // Listen for the "install" event, which indicates the
+      // process is available as a service.
+      svc.on('install', () => {
+        svc.start();
+      });
+
+      svc.install();
+      console.log('Instalation of service finished');
+    });
+})();
